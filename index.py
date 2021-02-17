b819a5f6fd3324cf0c8d88519b45afd1fc2dbc2f
chensxb97 <65479937+chensxb97@users.noreply.github.com>
2021-02-18 00:13:59 +0800

    BSBI Implementation
    Can start editing from line 111: # Merge posting lists

--- index.py
+++ index.py
@@ -31,11 +31,11 @@ def build_index(in_dir, out_dict, out_postings):
     list_punc = list(string.punctuation)
     stemmer = PorterStemmer()
     index_dict = {}
-    postings = {}
     files = [f for f in listdir(in_dir) if isfile(
         join(in_dir, f))]  # all files from directory
     sorted_files = sorted(files, key=lambda f: int(
         os.path.splitext(f)[0]))  # sorted files
+
     # Word_processing and tokenisation for each file
     for file in sorted_files:
         file_path = join(in_dir, file)
@@ -57,47 +57,107 @@ def build_index(in_dir, out_dict, out_postings):
         for token in tokens:
             if token in index_dict.keys():
                 # Account for repeated words in the same file
-                if int(file) not in postings[token]:
-                    index_dict[token] += 1
-                    postings[token].append(int(file))
+                if int(file) not in index_dict[token]:
+                    index_dict[token].append(int(file))
             else:
-                index_dict[token] = 1
-                postings[token] = [int(file)]
+                index_dict[token] = [int(file)]
 
     # Sort dictionary
     temp_index_dict = sorted(index_dict.items())
     # Note temp_index_dict is are key-sorted tuples of the indexed dictionary
-    sorted_index_dict = {}
-    for pointer, (key, value) in enumerate(temp_index_dict):
-        pointer += 1  # Pointer starts from 1
-        # value:doc-frequency, pointer:line number
-        sorted_index_dict[key] = (value, pointer)
+    sorted_index_dict = create_final_dict(temp_index_dict)
+    print(sorted_index_dict)
+
+    #Divide list of files to 10 batches(BSBI)
+    count = count_files(in_dir)
+    n = count//2 #Tested with 2 blocks
+    blocks = [sorted_files[x:x+n] for x in range(0, count, n)]
+    print('Batches: \n',blocks)
+
+    #Processing block
+    batch_number = 1
+    for block in blocks:
+        posting_dict={}
+        for file in block:
+            file_path = join(in_dir, file)
+            f = open(file_path, "r")
+            tokens = []
+            for line in f:
+                new_line = ''
+                for c in line:
+                    if c not in list_punc:
+                        new_line +=c
+                new_line = new_line.lower()
+                for sentence in nltk.sent_tokenize(new_line):
+                    for word in nltk.word_tokenize(sentence):
+                        word = stemmer.stem(word)
+                        tokens.append(word)
+            for token in tokens:
+                key = sorted_index_dict[token][0]
+                if key in posting_dict.keys():
+                    if int(file) not in posting_dict[key]:
+                        posting_dict[key].append(int(file))
+                else:
+                    posting_dict[key]=[int(file)]
+        #Create and save postings   
+        postings = create_posting_lists(posting_dict)
+        file_name = out_postings+'_{}.txt'.format(batch_number)
+        postings_out = open(file_name,'w')
+        for posting in postings:
+            posting_out.write(posting)
+        posting_out.close()     
+        batch_number+=1   
+
+    # Merge posting lists
+
+    '''To FILL IN'''
+
+    # Save dictionary
+    pickle.dump(sorted_index_dict, open(out_dict, "wb"))
+    print('done!')
+        
+''''
+OLD CODE
 
     # Save results postings file
     output_post = open(out_postings, "w")
-    for word in sorted_index_dict:
-        posting_list = create_posting_list(postings[word])
+    for token in sorted_index_dict:
+        posting_list = create_posting_lists(postings[word])
         output_post.write(posting_list + '\n')
     output_post.close()
 
     # Save dictionary
     pickle.dump(sorted_index_dict, open(out_dict, "wb"))
     print('done!')
+    
+'''
+def create_final_dict(dictionary):
+    sorted_dict = {}
+    start = 0
+    for termId, (key, value) in enumerate(dictionary):
+        # termId: termId, key: token, value: postings-array, (start, length): for reading posting lists using seek() function
+        doc_freq = len(value)
+        posting_str = ''
+        for file in value:
+            posting_str+= str(file)
+            posting_str+=' '
+        sorted_dict[key] = (termId, doc_freq,start,len(posting_str))
+        start+=len(posting_str)
+    return sorted_dict
 
+def count_files(dir):
+    return len([1 for x in list(os.scandir(dir)) if x.is_file()])
 
-def create_posting_list(posting):
-    posting_list = [str(i) for i in list(posting)]
-    interval = math.floor(math.sqrt(len(posting_list)))
-    posting_str = ''
-    i = 1
-    for p in posting_list:
-        if interval > 0 and i % interval == 0 and i != 1:
-            posting_str += '*{} '.format(interval)
-        posting_str += p
-        posting_str += ' '
-        i += 1
-    return posting_str
-
+def create_posting_lists(posting_dict):
+    sorted_postings = sorted(posting_dict.items())
+    posting_lists = []
+    for (termId, postingArr) in sorted_postings:
+        posting_str = ''
+        for i in postingArr:
+            posting_str+=str(i)
+            posting_str+=' '
+        posting_lists.append((posting_str))
+    return posting_lists
 
 input_directory = output_file_dictionary = output_file_postings = None
 
