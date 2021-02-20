a231e62bed43ee19f881c2f80bd56ba065697351
chensxb97 <65479937+chensxb97@users.noreply.github.com>
2021-02-20 23:49:03 +0800

    postingList Implementation done

--- search.py
+++ search.py
@@ -7,10 +7,9 @@ import math
 import pickle
 import os
 from nltk.stem.porter import PorterStemmer
-from postingList import postingList
+from postingList import postingList, ListNode
 
 # python search.py -d dictionary.txt -p postings.txt  -q queries.txt -o results.txt
-
 
 def usage():
     print("usage: " +
@@ -30,8 +29,10 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     in_dict = open(dict_file, 'rb')
     sorted_dict = pickle.load(in_dict)
     number_of_terms = len(sorted_dict.keys())
-    globalSet = set(range(1, number_of_terms))
-    # print(sorted_dict)
+    #print(sorted_dict)
+
+    # Global postingList
+    globalPostingList = postingList(list(range(1, number_of_terms))).addSkips() 
 
     # posting lists
     postings = open(postings_file, 'r')
@@ -78,7 +79,111 @@ def run_search(dict_file, postings_file, queries_file, results_file):
         # print('Queue: ', queue)
         # print('Stack: ', stack)
 
-        # process query (STACK IMPLEMENTATION)
+        # process query (postingListClass IMPLEMENTATION)
+        for item in queue:
+            if item not in operators:
+                stack.append(processItem(item,sorted_dict,postings)) # convert items to corresponding postingLists
+            elif item == 'NOT':
+                list1 = stack.pop()
+                stack.append(NOT(list1,globalPostingList)) # NOT
+            elif item == 'OR' or item =='AND':
+                list1 = stack.pop()
+                list2 = stack.pop()
+                if item =='OR':
+                    stack.append(OR(list1,list2)) # OR
+                else: 
+                    stack.append(AND(list1, list2)) # AND
+        i+=1
+        #print('Final Stack: ',stack[0].convertToList()) # Result
+
+# extract postingList from dictionary 
+def processItem(item,sorted_dict, postings):
+    if item == None:
+        print('Item is empty')
+        return None
+    else:
+        if item in sorted_dict.keys():
+            postings.seek(sorted_dict[item][2],0)
+            posting_str = (postings.read(sorted_dict[item][3]))
+            posting_l = postingList(posting_str).addSkips()
+    return posting_l
+
+def NOT(list1, globalPostingList):
+    result = postingList()
+    cur1 = list1.head
+    curGlobal = globalPostingList.head
+    while cur1!= None:
+        # traverse global list if the global ids are less than list id
+        while cur1 != None and curGlobal.doc_id <cur1.doc_id:
+            result.insert(ListNode(curGlobal.doc_id))
+            curGlobal = curGlobal.next
+        # Insert ids if ids are not the same
+        if curGlobal.doc_id != cur1.doc_id:
+            result.insert(ListNode(curGlobal.doc_id))
+        # Do nothing if ids are the same
+        curGlobal = curGlobal.next
+        cur1 = cur1.next
+    # traverse the remaining elements of the global list
+    while curGlobal!=None:
+        result.insert(ListNode(curGlobal.doc_id))
+        curGlobal = curGlobal.next
+    return result.addSkips()
+
+def AND(list1,list2):
+    result = postingList()
+    cur1 = list1.head
+    cur2 = list2.head
+    while cur1 != None and cur2 != None:
+        # skip to next node if the id of skipped node is less than the id of the compared node
+        while cur1.skip_to != None and cur1.skip_to.doc_id < cur2.doc_id and cur1.doc_id != cur2.doc_id:
+            cur1 = cur1.skip_to
+        while cur2.skip_to != None and cur2.skip_to.doc_id < cur1.doc_id and cur1.doc_id != cur2.doc_id:
+            cur2 = cur2.skip_to
+        # insert ids if they are the same
+        if cur1.doc_id == cur2.doc_id:
+            result.insert(ListNode(cur1.doc_id))
+            cur1=cur1.next
+            cur2=cur2.next
+        # traversal
+        elif cur1.doc_id < cur2.doc_id:
+            cur1 = cur1.next
+        else:
+            cur2 = cur2.next
+    return result.addSkips() 
+    
+def OR(list1,list2):
+    result = postingList()
+    cur1 = list1.head
+    cur2 = list2.head
+    while cur1 != None and cur2 != None:
+        # insert id if ids are the same
+        if cur1.doc_id == cur2.doc_id:
+            result.insert(ListNode(cur1.doc_id))
+            cur1=cur1.next
+            cur2=cur2.next
+        else:
+            # traverse through the first list if the doc-ids are less than the second list's second doc-id
+            while cur1!=None and cur2!=None and cur1.doc_id < cur2.doc_id:
+                result.insert(ListNode(cur1.doc_id))
+                cur1 = cur1.next
+            while cur2!= None and cur1!=None and cur2.doc_id < cur1.doc_id:
+                result.insert(ListNode(cur2.doc_id))
+                cur2 = cur2.next
+
+    # traverse the remaining elements of the other list
+    if cur1==None:
+        while cur2!=None:
+            result.insert(ListNode(cur2.doc_id))
+            cur2 = cur2.next
+    if cur2==None:
+        while cur1!=None:
+            result.insert(ListNode(cur1.doc_id))
+            cur1=cur1.next 
+    return result.addSkips()
+
+
+'''
+        # process query (SET IMPLEMENTATION)
         for item in queue:
             # obtain posting lists from tokens
             if item not in operators:
@@ -102,7 +207,6 @@ def run_search(dict_file, postings_file, queries_file, results_file):
         i += 1
         print('Final Stack: ', sorted(stack[0]))
 
-
 def turnIntoSet(item, sorted_dict, postings):
     if type(item) is not set:
         if item in sorted_dict.keys():
@@ -116,18 +220,15 @@ def turnIntoSet(item, sorted_dict, postings):
         tokenSet = item
     return tokenSet
 
-
-def OR(tokenSet1, tokenSet2):
-    return tokenSet1 | tokenSet2
-
-
 def NOT(tokenSet1, globalSet):
     return (globalSet - tokenSet1)
-
 
 def AND(tokenSet1, tokenSet2):
     return tokenSet1 & tokenSet2
 
+def OR(tokenSet1, tokenSet2):
+    return tokenSet1 | tokenSet2
+'''
 
 dictionary_file = postings_file = file_of_queries = output_file_of_results = None
 
