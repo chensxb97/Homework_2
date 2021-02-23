b81bbd2bcdbe8e4a876064c5207ff661e9fc9af3
chensxb97 <65479937+chensxb97@users.noreply.github.com>
2021-02-23 21:53:54 +0800

    Modified

--- postingList.py
+++ postingList.py
@@ -2,48 +2,51 @@ import math
 
 class ListNode:
     # Initialisation
-    def __init__(self, doc_id=None):
-        self.doc_id = int(doc_id)
-        self.next = None
-        self.skip_to = None
+    def __init__(self, doc_id):
+        self.doc_id = int(doc_id) # docId
+        self.next = None # Points to next node
+        self.skip_to = None # Points to skipped node
 
 class postingList:
     # Initialisation
-    def __init__(self, postingStr=None):
-        self.has_skips = False
-        self.length = 0
-        self.head = None
-        self.tail = None
+    def __init__(self, postingStr):
+        self.head = None # head pointer
+        self.tail = None # tail pointer
+        self.length = 0 # length of list
+        self.has_skips = False # checks if list has added skips
 
         # Populate postingList
         if postingStr != None:
             if type(postingStr) == str: # Input argument of type: string
                 doc_ids = [int(i) for i in postingStr.split()]
-                doc_ids.sort()
                 for doc_id in doc_ids:
                     self.insert(ListNode(doc_id))
             elif type(postingStr) == list or type(postingStr) == set: # Input argument of type: list or set
-                for doc_id in postingStr:
-                    self.insert(ListNode(doc_id))
+                for int(doc_id) in postingStr:
+                    self.insert(ListNode(int(doc_id)))
+            else:
+                print('Invalid input.')
+        else:
+            print('Empty input')
 
     # Insertion at back of list
     def insert(self, node):
-        if self.head == None:
+        if self.head == None: # Empty list
             self.head = node
             self.tail = node
         else:
-            self.tail.next = node
+            self.tail.next = node # Append node to list
             self.tail = node
-        self.length += 1
+        self.length += 1 # Increment length
 
     # Convert object to string
     def convertToString(self):
         result = ''
         cur = self.head
         while cur != None:
-            result += str(cur.doc_id) + ' '
+            result += str(cur.doc_id) + ' ' 
             cur = cur.next
-        result = result.rstrip()
+        result = result.rstrip() # Remove trailing whitespaces on the right
         return result
 
     # Gets node via search index
@@ -52,12 +55,12 @@ class postingList:
         while index > 0:
             cur = cur.next
             index -= 1
-        return cur
+        return cur 
 
     # Calculates skip interval based on the postingList length
     def skipInterval(self):
-        skip_interval = math.floor(math.sqrt(self.length))
-        return skip_interval
+        return math.floor(math.sqrt(self.length))
+        
 
     # Sets the skip pointer of node1 to node2
     def skip(self, idx1, idx2):
@@ -72,10 +75,10 @@ class postingList:
             skips = self.skipInterval()
             node2 = skips
             while node2 < self.length:
-                self.skip(node1, node2)
+                self.skip(node1, node2) # Assigns node 1 a skip pointer to node 2
                 node1 = node2
                 node2 += skips
-            self.has_skips = True
+            self.has_skips = True # List is now with added skips
         else:
             print('Posting list already added skips')
         return self
